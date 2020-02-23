482bc6311141591ae9f4052fb0970a5dd4f70c85
wilwong89 <wilwong89@gmail.com>
2020-02-23 15:05:47 -0800

    Beginning work on navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-container flex f-s-between">
+    <div class="nav-item nav-corner-icon"></div>
+    <div class="nav-item nav-links"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {},
+  methods: {}
+};
+</script>
+
+<style>
+.nav-container {
+  height: 100px;
+  width: 100%;
+  background-color: lightseagreen;
+}
+
+.nav-item {
+  height: 100%;
+  line-height: 100px;
+}
+</style>
