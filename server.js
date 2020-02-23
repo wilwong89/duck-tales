b6bb814dee1d1df82524c3e121e9ef00f73fe298
wilwong89 <wilwong89@gmail.com>
2020-02-23 14:34:00 -0800

    Setup connection to postgres

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-require("dotenv").config();
 
-const app = express();
+const { Pool } = require("pg");
+const dbUri = {
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URI}`
+};
+const pool = new Pool(dbUri);
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -12,7 +18,7 @@ app.use(bodyParser.json());
 app.use(express.static("dist"));
 
 app.get("/test", (req, res) => {
-  res.send("test");
+  res.send("SELECT * FROM TestColumns");
 });
 
 const port = process.env.PORT || 4000;
